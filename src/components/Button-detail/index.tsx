1e4dd7df9daadf59ad0879635050042c6dce6f82
cleytonRR <cleytonrodrigues69@live.com>
2021-10-03 19:40:55 -0300

    Refactor button

--- src/components/Button-detail/index.tsx
+++ src/components/Button-detail/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import { ArrowRight } from "react-feather";
 import styles from "./styles.module.css";
 
 const ButtonDetail = () => {
-  return <button className={styles.button_list}>Lista de moedas {"->"}</button>;
+  return (
+    <button className={styles.button_list}>
+      Lista de moedas <ArrowRight />
+    </button>
+  );
 };
 
 export default ButtonDetail;
