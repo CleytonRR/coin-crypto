d11ac22b74619f8c8ab84f1ac6a8701487d9024f
cleytonRR <cleytonrodrigues69@live.com>
2021-10-03 00:58:41 -0300

    Added logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+const Logo = () => {
+  return (
+    <h2 className={styles.logo}>
+      <span>C</span>
+      oin <span>C</span>
+      rypto
+    </h2>
+  );
+};
+
+export default Logo;
