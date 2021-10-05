f584fe0031e353fc003e1ddb38eaaca0b9de5051
cleytonRR <cleytonrodrigues69@live.com>
2021-10-04 23:32:14 -0300

    Install eslint-plugin-react-hooks

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,62 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Footer from "components/Footer";
 import Menu from "components/Menu";
 import Container from "components/Container";
 
-import bitcoin from "assets/bitcoin.png";
-
 import styles from "./styles.module.css";
+import api from "services/api";
+
+export interface Coin {
+  id: string;
+  name: string;
+  price: number;
+  icon: string;
+  twitterUrl?: string;
+  websiteUrl?: string;
+  exp: string[];
+}
+
+interface CoinDataAxios {
+  coins: Coin[];
+}
 
 const List = () => {
+  const [coins, setCoins] = useState<Coin[]>();
+
+  useEffect(() => {
+    async function getCoins() {
+      const { data } = await api.get<CoinDataAxios>("");
+
+      setCoins(data.coins);
+    }
+
+    getCoins();
+  }, []);
+
+  const formatPrice = (price: number) => {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price);
+  };
+
   return (
     <>
       <Menu />
       <Container>
         <h1 className={styles.title}>Principais cryptos</h1>
         <section className={styles.cards}>
-          <div className={styles.card}>
-            <img src={bitcoin} alt="Bitcoin logo" />
-            <h3>Bitcoin</h3>
-            <h5>USD 45215.00</h5>
-            <a href="">Ver detalhes</a>
-          </div>
-          <div className={styles.card}>
-            <img src={bitcoin} alt="Bitcoin logo" />
-            <h3>Bitcoin</h3>
-            <h5>USD 45215.00</h5>
-            <a href="">Ver detalhes</a>
-          </div>
-          <div className={styles.card}>
-            <img src={bitcoin} alt="Bitcoin logo" />
-            <h3>Bitcoin</h3>
-            <h5>USD 45215.00</h5>
-            <a href="">Ver detalhes</a>
-          </div>
-          <div className={styles.card}>
-            <img src={bitcoin} alt="Bitcoin logo" />
-            <h3>Bitcoin</h3>
-            <h5>USD 45215.00</h5>
-            <a href="">Ver detalhes</a>
-          </div>
-          <div className={styles.card}>
-            <img src={bitcoin} alt="Bitcoin logo" />
-            <h3>Bitcoin</h3>
-            <h5>USD 45215.00</h5>
-            <a href="">Ver detalhes</a>
-          </div>
-          <div className={styles.card}>
-            <img src={bitcoin} alt="Bitcoin logo" />
-            <h3>Bitcoin</h3>
-            <h5>USD 45215.00</h5>
-            <a href="">Ver detalhes</a>
-          </div>
-          <div className={styles.card}>
-            <img src={bitcoin} alt="Bitcoin logo" />
-            <h3>Bitcoin</h3>
-            <h5>USD 45215.00</h5>
-            <a href="">Ver detalhes</a>
-          </div>
+          {coins?.map((coin) => (
+            <div className={styles.card} key={coin.id}>
+              <img src={coin.icon} alt={coin.name} />
+              <h3>{coin.name}</h3>
+              <h5>{formatPrice(coin.price)}</h5>
+              <a href="">Ver detalhes</a>
+            </div>
+          ))}
         </section>
       </Container>
       <Footer />
