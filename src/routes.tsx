af05dd67fbaf2134896369bd4c0d0956ea93533b
cleytonRR <cleytonrodrigues69@live.com>
2021-10-07 01:43:52 -0300

    Added trasnition in routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+import "utils/animation-routes/animation.module.css";
 
 import Home from "pages/Home";
 import List from "pages/List";
@@ -8,17 +11,25 @@ import Detail from "pages/Detail";
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/list" exact>
-          <List />
-        </Route>
-        <Route path="/detail/:coinname">
-          <Detail />
-        </Route>
-      </Switch>
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={3000} classNames="fade">
+              <Switch>
+                <Route path="/" exact>
+                  <Home />
+                </Route>
+                <Route path="/list" exact>
+                  <List />
+                </Route>
+                <Route path="/detail/:coinname">
+                  <Detail />
+                </Route>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     </Router>
   );
 }
