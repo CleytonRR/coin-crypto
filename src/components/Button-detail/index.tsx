afe6c279e6603c119fd3f797cceb2028fb9e06ba
cleytonRR <cleytonrodrigues69@live.com>
2021-10-10 17:29:01 -0300

    added link component to redirect to list

--- src/components/Button-detail/index.tsx
+++ src/components/Button-detail/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { ArrowRight } from "react-feather";
 import styles from "./styles.module.css";
 
 const ButtonDetail = () => {
   return (
-    <button className={styles.button_list}>
-      Lista de moedas <ArrowRight />
-    </button>
+    <Link to="/list">
+      <button className={styles.button_list}>
+        Lista de moedas <ArrowRight />
+      </button>
+    </Link>
   );
 };
 
